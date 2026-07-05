66ba91ffcfb22c6aacde88f4428ad3b314a0d8f7
Ashish <ashish@gmail.com>
2026-07-05 18:21:35 +0530

    new

--- src/Features/CartSlice.jsx
+++ src/Features/CartSlice.jsx
@@ -1,47 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit"; 
 
 const initialState = {
-    cartItems: [],
+    CartItems: [],
 };
 
-const cartSlice = createSlice({
-    name: "cart",
+const CartSlice = createSlice({
+    name: "Cart",
     initialState,
     reducers: {
         addToCart: (state, action) => {
-            const itemIndex = state.cartItems.findIndex((item) => item.id === action.payload.id);
+            const itemIndex = state.CartItems.findIndex((item) => item.id === action.payload.id);
             if (itemIndex >= 0) {
-                state.cartItems[itemIndex].quantity += 1;
+                state.CartItems[itemIndex].quantity += 1;
             } else {
                 const tempProduct = { ...action.payload, quantity: 1 };
-                state.cartItems.push(tempProduct);
+                state.CartItems.push(tempProduct);
             }   
         },
         removeFromCart: (state, action) => {
             const id = action.payload.id || action.payload;
-            state.cartItems = state.cartItems.filter((item) => item.id !== id);
+            state.CartItems = state.CartItems.filter((item) => item.id !== id);
         },
         increment: (state, action) => {
             const id = action.payload.id || action.payload;
-            const itemIndex = state.cartItems.findIndex((item) => item.id === id);
+            const itemIndex = state.CartItems.findIndex((item) => item.id === id);
             if (itemIndex >= 0) {
-                state.cartItems[itemIndex].quantity += 1;
+                state.CartItems[itemIndex].quantity += 1;
             }
         },
         decrement: (state, action) => {
             const id = action.payload.id || action.payload;
-            const itemIndex = state.cartItems.findIndex((item) => item.id === id);
+            const itemIndex = state.CartItems.findIndex((item) => item.id === id);
             if (itemIndex >= 0) {
-                if (state.cartItems[itemIndex].quantity > 1) {
-                    state.cartItems[itemIndex].quantity -= 1;
+                if (state.CartItems[itemIndex].quantity > 1) {
+                    state.CartItems[itemIndex].quantity -= 1;
                 } else {
                  
-                    state.cartItems = state.cartItems.filter((item) => item.id !== id);
+                    state.CartItems = state.CartItems.filter((item) => item.id !== id);
                 }
             }
         }
     }
 });
 
-export const { addToCart, removeFromCart, increment, decrement } = cartSlice.actions;
-export default cartSlice.reducer;
+export const { addToCart, removeFromCart, increment, decrement } = CartSlice.actions;
+export default CartSlice.reducer;
